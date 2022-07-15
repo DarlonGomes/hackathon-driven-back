9fdd15533c2865afed5af8cf9ec234a5d67a7193
Daniel Lucas Ederli <danielnatsufire@gmail.com>
2022-07-15 12:50:09 -0300

    feat: user authentication, signin and signup

---
+++ src/controllers/authController.js
@@ -0,0 +1,35 @@
+import db from "../database/db.js"
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+
+async function signUp(req, res) {
+    const { name, email, password } = req.body
+
+    try {
+        const hashPassword = bcrypt.hashSync(password, 10)
+
+        await db.collection('users').insertOne({ name, email, password: hashPassword })
+
+        res.sendStatus(201)
+    } catch(err) {
+        res.status(500).send(err)
+    }
+}
+
+async function signIn(req, res) {
+    const { user } = res.locals
+
+    try {
+        if(bcrypt.compareSync(req.body.password, user.password)) {
+            const token = jwt.sign(user, process.env.SECRET_KEY, { expiresIn: '7d' })
+
+            res.status(200).send({ name: user.name, token })
+        } else {
+            res.status(403).send('wrong password')
+        }
+    } catch (err) {
+        res.status(500).send(err)
+    }
+}
+
+export { signUp, signIn }
